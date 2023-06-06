46830af7ddbfdb9b54cd168268aed0a30d35693c
Vanessa Castillo Garcia <castillogarciav11@gmail.com>
2023-06-06 12:50:03 -0600

    segundo cambio

--- segunda.java
+++ segunda.java
@@ -1,15 +1,15 @@
-public class Suma {
+public class Resta {
 public static void main(String args[]){
 
 
-int numero1 = 5;
-int numero2 = 2;
+int numero1 = 24;
+int numero2 = 82;
 int resultado = 0;
   
 
-resultado = numero1+numero2;
+resultado = numero1-numero2;
 
-System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+System.out.println("La suma es " + numero1 + " - " + numero2 + " = " + resultado);
 
 }
 }
