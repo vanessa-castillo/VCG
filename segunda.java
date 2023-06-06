fee164dbb13a2e1faf6b582bcfd1e3cc45fa06ba
Vanessa Castillo Garcia <castillogarciav11@gmail.com>
2023-06-06 12:43:26 -0600

    primer cambio

---
+++ segunda.java
@@ -0,0 +1,15 @@
+public class Suma {
+public static void main(String args[]){
+
+
+int numero1 = 5;
+int numero2 = 2;
+int resultado = 0;
+  
+
+resultado = numero1+numero2;
+
+System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+
+}
+}
